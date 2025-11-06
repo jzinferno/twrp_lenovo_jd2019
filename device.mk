605a4c3c2b4fce7cfec320a703d475d97361481d
jzinferno <121882759+jzinferno@users.noreply.github.com>
2025-11-06 12:28:22 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -21,6 +21,8 @@ PRODUCT_PACKAGES += \
 
 # Fastbootd
 PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd
 
 # Additional binaries & libraries needed for recovery
